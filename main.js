b7558960183fbef87f4dcbfb7b6d9822b5ba8d01
DS <cckn1985@gmail.com>
2018-02-06 13:29:10 +0900

    learning git

--- main.js
+++ main.js
@@ -1,4 +1,23 @@
 $(document).ready(function() {
   'use strict';
-  console.log('main.js loaded');
+  paper.install(window);
+  paper.setup(document.getElementById('mainCanvas'));
+
+  var tool = new Tool();
+
+  var c = Shape.Circle(200, 200, 80);
+  c.fillColor = 'black';
+
+  var text = new PointText(200, 200);
+  text.justification = 'center';
+  text.fillColor = 'white';
+  text.fontSize = 20;
+  text.content = 'hello world';
+
+  tool.onMouseDown = function(event) {
+    var c = Shape.Circle(event.point, 20);
+    c.fillColor = 'red ';
+  };
+
+  paper.view.draw();
 });
