189cf7bf5b5eef6b37dc07da6cd1fa788fcde593
DS <cckn1985@gmail.com>
2018-02-12 18:07:12 +0900

    러닝 자바스크립트 학습 중 260 pages

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
-const arr = Array(10).map(function(x) {
-    return 5;
-});
+// Learning JavaScript
+// chapter 12 : Iterator and Generator
+
+// Iterator
+
+const book = [
+    'Twinkle, twinkle, little bat!',
+    "How I wonder what you're at!",
+    'Up abobe the world you fly',
+    'Like a tea tray in the sky.',
+    'Twinkle, twinkle, little bat!',
+    "How I wonder you're at!",
+];
+
+const it = book.values();
